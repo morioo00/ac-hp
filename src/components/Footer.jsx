ee0add59fa63c23d874a7d8a973a55a2f0499a88
morioo00 <morita.kazuki.se@gmail.com>
2026-02-17 20:11:01 +0900

    Redirect to case list after admin login

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -31,6 +31,7 @@ const Footer = () => {
     if (password === siteConfig.adminPassword) {
       setIsAdmin(true);
       closeLogin();
+      window.location.hash = "#cases";
       return;
     }
 
